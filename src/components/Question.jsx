0b720a175f79f6cdda99bdda9b5f539bc93ade99
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-03-30 15:00:39 +0200

    Added state control for the buttons.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,29 +1,52 @@
+import { useState } from "react";
 import Answers from "./Answers";
 import QuestionTimer from "./QuestionTimer";
+import QUESTIONS from "../questions";
 
-export default function Question({
-  questionText,
-  answers,
-  onSelectAnswer,
-  onSkipAnswer,
-  selectedAnswer,
-  answerState,
-}) {
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
+
   return (
     <div id="question">
-      <QuestionTimer
-        timeout={10000}
-        onTimeout={onSkipAnswer}
-      />
-      <p>{questionText}</p>
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <p>{QUESTIONS[index].text}</p>
       {/* When I move this logic in the separate component and add key property
             this component will rerender
          */}
       <Answers
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
