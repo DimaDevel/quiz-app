00040e823a1d90ff76d5278c45f5ba709479c4ab
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-03-31 15:10:59 +0200

    Added Summary component.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,6 +9,16 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     isCorrect: null,
   });
 
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -37,7 +47,12 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <p>{QUESTIONS[index].text}</p>
       {/* When I move this logic in the separate component and add key property
             this component will rerender
